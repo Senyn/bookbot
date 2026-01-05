5116d3d879a1278c566c111e24897298f36d0c9c
Senyn <cxianjie94@gmail.com>
2026-01-05 21:47:38 +0800

    H: update main

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
+from stats import get_word_count
+
 def get_book_text():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     return file_contents
 
 contents = get_book_text()
-
-def get_word_count(contents):
-    words = contents.split()
-    count = len(words)
-    return print(f"Found {count} total words")
 
 get_word_count(contents)
     
